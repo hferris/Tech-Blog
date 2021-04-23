22a520e9e010cd765f4ca0a7a15ee5cbf4c72146
Hanna Ferris <hferris@dbq.edu>
2021-04-23 17:54:37 -0500

    coded newpost.js

---
+++ public/js/newpost.js
@@ -0,0 +1,22 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector("#title").value.trim();
+    const content = document.querySelector("#content").value.trim();
+  
+    if (title && content) {
+      const response = await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({ title, content }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {document.location.replace('/');
+      } else {alert("Failed generate new post");
+      }
+    }
+  };
+  
+  document
+    .querySelector(".login-form")
+    .addEventListener("submit", loginFormHandler);
