9d0638f63df8e06ad602580d5da4d0030b2d8838
Hanna Ferris <hferris@dbq.edu>
2021-04-22 22:15:01 -0500

    added get routes

--- controllers/dashboard-Routes.js
+++ controllers/dashboard-Routes.js
@@ -1,12 +1,44 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../models/");
+const { response } = require('express');
 const withAuth = require("../utils/auth");
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.findAll({
+        attributes: ['id', 'title','textSpot'],
+        include: [{ model: comment }]
+      });
+  
+      const posts = postData.map((project) => project.get({ plain: true }));
+  
+      res.render('homepage', {
+        users,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
+
+
 
 
 // We need this later on 
-res.render("homepage", {
-    users,
-    logged_in: req.session.logged_in,
+// res.render("homepage", {
+//     users,
+//     logged_in: req.session.logged_in,
 
 
 
